ebbf56ac4d0e46ae1eff5bc0a27c00a41542b483
Arthur Falcão <arthurjvfalcao@gmail.com>
2020-06-11 23:40:10 +0100

    :rocket: add insert a disease

--- Main.hs
+++ Main.hs
@@ -14,6 +14,10 @@ type Connections = [String]
 
 type Disease = (Name, Virus, Symptoms, Quarantine)
 
+type Patient = (Name, Symptoms, Date)
+
+type PatientQuarantine = (Name, String, EndDate, Connections)
+
 listDiseases :: [Disease]
 listDiseases =
   [ ("hepatiteA", "picorna", ["icterícia", "fadiga", "febre", "mialgia"], False),
@@ -21,15 +25,27 @@ listDiseases =
     ("sarampo", "paramyxo", ["manchas", "erup¸c~oes", "tosse", "febre"], True)
   ]
 
-type Patient = (Name, Symptoms, Date)
-
 listPatients :: [Patient]
 listPatients =
   [ ("Joao", ["tosse", "dispneia"], (2020, 4, 2)),
     ("Ana", ["icterícia", "mialgia"], (2020, 4, 8))
   ]
 
-type PatientQuarantine = (Name, String, EndDate, Connections)
-
 listQuarantine :: [PatientQuarantine]
 listQuarantine = [("Joao", "covid_19", (2020, 5, 25), ["Maria", "Peter"])]
+
+-- insert new disease
+insertDisease :: IO ()
+insertDisease = do
+  putStr "Name: "
+  n <- getLine
+  putStr "Virus: "
+  v <- getLine
+  putStr "Symptoms: "
+  s <- getLine
+  putStr "Quarantine: "
+  q <- getLine
+  appendFile "diseases.txt" (n ++ "\t" ++ v ++ "\t" ++ s ++ "\t" ++ q ++ "\n")
+  putStr "Insert another one? (y or n) "
+  resp <- getLine
+  if (resp == "y" || resp == "Y") then insertDisease else return ()
